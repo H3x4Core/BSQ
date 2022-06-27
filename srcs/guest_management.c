a1bb765ec86126dfbec2ab635846f80fba1c0510
Hexa_Core <hexa_core@bluewin.ch>
2022-06-27 20:46:54 +0200

    update

--- srcs/guest_management.c
+++ srcs/guest_management.c
@@ -3,20 +3,45 @@
 /*                                                        :::      ::::::::   */
 /*   guest_management.c                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mpouce <mpouce@42lausanne.ch>              +#+  +:+       +#+        */
+/*   By: matwinte <marvin@42lausanne.ch>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/27 18:59:53 by mpouce            #+#    #+#             */
-/*   Updated: 2022/06/27 18:59:53 by mpouce           ###   ########.fr       */
+/*   Updated: 2022/06/27 20:24:37 by matwinte         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "header.h"
+
+char	*ft_strcat(char *dest, char *src)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	j = 0;
+	while (dest[i])
+		i++;
+	while (src[j])
+		dest[i++] = src[j++];
+	dest[i] = 0;
+	return (dest);
+}
+
 char	*read_funny_words(void)
 {
+	char	buffer[BUF_SIZE + 1];
 	int		len;
 	char	*str;
 	
-	str = (char *)malloc(4096 + 1);
-	len = read(0, str, 4096);
-	str[len - 1] = '\0';
+	len = 1;
+	str = (char *)malloc(BUF_SIZE + 1);
+	if (!str)
+		return (0);
+	while (len > 0)
+	{
+		len = read(0, buffer, BUF_SIZE);
+		if (len > 0)
+			ft_strcat(str, buffer);
+	}
 	return (str);
 }
